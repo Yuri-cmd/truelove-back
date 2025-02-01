779e85deb750fbb526e3ae4f6e2e4807deffdaeb
Yuri Marroquin Mejia <84044360+Yuri-cmd@users.noreply.github.com>
2025-02-01 13:04:59 -0500

    corregir eloquent

--- app/Http/Controllers/PedidoTrackingController.php
+++ app/Http/Controllers/PedidoTrackingController.php
@@ -9,8 +9,7 @@ class PedidoTrackingController extends Controller
 {
     public function obtenerEstado($id)
     {
-        $pedido = PedidoTracking::where('pedido_id', $id);
-        dd($pedido);
+        $pedido = PedidoTracking::where('pedido_id', $id)->latest()->first();
         if (!$pedido) {
             return response()->json(['error' => 'Pedido no encontrado'], 404);
         }
