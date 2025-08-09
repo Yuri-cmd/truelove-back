0ef85f25cc78278f1cd62d22c56ef5b0c2e446f2
Yuri-cmd <yurim011218@gmail.com>
2025-08-09 11:07:51 -0500

    Replace environment variables with hardcoded Twilio credentials in TwilioService

--- app/Services/TwilioService.php
+++ app/Services/TwilioService.php
@@ -12,8 +12,8 @@ class TwilioService
     public function __construct()
     {
         $this->twilio = new Client(
-            env('TWILIO_SID'),
-            env('TWILIO_AUTH_TOKEN')
+            '***TWILIO_SID_REMOVED***',
+            '***TWILIO_TOKEN_REMOVED***'
         );
     }
 
@@ -21,7 +21,7 @@ class TwilioService
     {
         try {
             $this->twilio->messages->create($to, [
-                'from' => env('TWILIO_PHONE_NUMBER'),
+                'from' => '+17756287966',
                 'body' => $message,
             ]);
             return true;
