fd58e0b33cc061d3f597e60b3b27a4a23a623e96
Yuri-cmd <yurim011218@gmail.com>
2025-08-11 10:23:29 -0500

    Rename JWT_SECRET configuration key to jwt_secret for consistency

--- app/Http/Controllers/RevisarDatosController.php
+++ app/Http/Controllers/RevisarDatosController.php
@@ -21,7 +21,7 @@ class RevisarDatosController extends Controller
             
             if ($token) {
                 // Decodificar el token para obtener el registration_id
-                $decoded = JWT::decode($token, new Key('P8zs3vF2xR9tN7yJ4mQ6bK1hG5wC0lA', 'HS256'));
+                $decoded = JWT::decode($token, new Key(config('app.jwt_secret'), 'HS256'));
                 $registrationId = $decoded->registration_id;
                 
                 $businessRegistration = BusinessRegistration::find($registrationId);
